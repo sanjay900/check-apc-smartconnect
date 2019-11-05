4782b3d1a6b554ab42d9896105a003a6c994b739
Sanjay Govind <sanjay.govind9@gmail.com>
2019-11-05 14:06:57 +1300

    Add basic script that can be used by icinga

---
+++ apc-smartconnect.pl
@@ -0,0 +1,95 @@
+#! /usr/bin/perl -w
+use strict;
+use warnings;
+use LWP::UserAgent;
+use HTTP::Request::Common;
+use HTTP::Cookies;
+use JSON;
+my $id = shift;
+my $username = shift;
+my $password = shift;
+# Store all cookies, we need them for auth. Cache them in a file, that way, we can reuse them across sessions
+my $jar = HTTP::Cookies->new( 
+    file => '/tmp/apc-cookies.txt',
+    autosave => 1,
+    ignore_discard => 1,
+);
+# Create an agent that we can for web requests
+my $ua = LWP::UserAgent->new(cookie_jar => $jar);
+# Try and fetch UPS data
+my $res = $ua->request(GET "https://smartconnect.apc.com/api/v1/gateways/$id");
+my $ups_core_data = decode_json($res->decoded_content);
+# If error is defined, than we don't have permission and need to log in.
+if (defined $ups_core_data->{'error'}) {
+    # Navigate to login page
+    my $res = $ua->request(GET 'https://smartconnect.apc.com/auth/login');
+    # The login page sets a bunch of cookies using javascript. Fake that using a regex.
+    my $str = $res->decoded_content;
+    while($str =~ /document.cookie\s*=\s+"([^"]+)=([^"]+);Path=(.);(secure)?";/g) {
+        $jar->set_cookie(0, $1, $2, $3, "secureidentity.schneider-electric.com");
+    }
+    # The login page then redirects using javascript. Fake that using a regex.
+    my @redir = $res->decoded_content =~ /window.location\s*=\s+"([^"]+)";/g;
+    $res = $ua->request(GET "https://secureidentity.schneider-electric.com/@redir");
+
+    # Construct a form that we can submit for login
+    my %data = (
+        "j_id0:j_id45" => "j_id0:j_id45",
+        "j_id0:j_id45:j_id58" => "j_id0:j_id45:j_id58",
+        'AJAXREQUEST' => '_viewRoot',
+        'usrname' => $username,
+        'password' => $password,
+        'com.salesforce.visualforce.ViewStateMAC' => $res->decoded_content =~ /"com.salesforce.visualforce.ViewStateMAC"\s+value="([^"]+)"/g,
+        'com.salesforce.visualforce.ViewStateVersion' => $res->decoded_content =~ /"com.salesforce.visualforce.ViewStateVersion"\s+value="([^"]+)"/g,
+        'com.salesforce.visualforce.ViewState' => $res->decoded_content =~ /"com.salesforce.visualforce.ViewState"\s+value="([^"]+)"/g,
+    );
+    # Submit that form
+    $res = $ua->request(POST 'https://secureidentity.schneider-electric.com/identity/UserLogin', [ %data ]);
+
+    # UserLogin returns a weird client based redirect, so we need to act on it manually
+    @redir = $res->decoded_content =~ /<meta\s+name="Location"\s+content="([^"]+)" \/>/g;
+    $res = $ua->request(GET @redir);
+
+    # Then that page uses javascript to redirect, so we need to follow that manually with a regex
+    @redir = $res->decoded_content =~ /window.location.href\s*='([^']+)';/g;
+
+    # Then that page uses javascript to redirect, so we need to follow that manually with a regex
+    $res = $ua->request(GET "https://secureidentity.schneider-electric.com/@redir");
+    @redir = $res->decoded_content =~ /window.location.href\s*='([^']+)';/g;
+    $res = $ua->request(GET @redir);
+    # And now, finally, we can request information about a ups
+    $res = $ua->request(GET "https://smartconnect.apc.com/api/v1/gateways/$id");
+    $ups_core_data = decode_json($res->decoded_content);
+}
+# And information about
+$res = $ua->request(GET "https://smartconnect.apc.com/api/v1/gateways/$id?collection=input,output,battery,network");
+# upsOperatingMode
+my $ups_other_data = decode_json($res->decoded_content);
+my $mode = $ups_core_data->{'status'}{'upsOperatingMode'};
+my $runtime = $ups_other_data->{'battery'}{'runtimeRemaining'}/60;
+my $status = "- LOAD $ups_other_data->{'output'}{'loadRealPercentage'}\% - TEMP $ups_other_data->{'battery'}{'temperature'}C - BAT $ups_other_data->{'battery'}{'chargeStatePercentage'}\% - RUNTIME $runtime MIN";
+if ($mode eq "online") {
+    print "OK $status\n";
+    exit(0);
+} elsif ($mode eq "onbattery.rtc") {
+    print "OK - UPS On Battery for Calibration - $status";
+    exit(0);
+} elsif ($mode eq "bypass_ups_init") {
+    print "WARNING - UPS Power Bypass, UPS Initiated - $status";
+    exit(1);
+} elsif ($mode eq "bypass_user_init") {
+    print "WARNING - UPS Power Bypass, User Initiated - $status";
+    exit(1);
+} elsif ($mode eq "onbattery") {
+    print "WARNING - UPS On Battery - $status";
+    exit(1);
+} elsif ($mode eq "onbattery.low_battery") {
+    print "WARNING - UPS Low Battery - $status";
+    exit(1);
+} elsif ($mode eq "ups_off") {
+    print "WARNING - UPS Output Power Turned Off - $status";
+    exit(1);
+} else {
+    print "CRITICAL - UPS Offline";
+    exit(2);
+}
